a3e3f424d8fda68e8d3a38d79efe1e15ec201755
zm00622 <zarymanning@gmail.com>
2020-02-18 18:32:14 -0500

    This commit copies files over from the repository geometric-theme. Those files will serve as the base/starting point of this project. To see the earliest commits that went into making this theme, go view the repository named “portfolio-website” and follow the development from there.

---
+++ build/precache-manifest.6030a72e62cc2a4d98095c3367bc8dd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8b86f88b902e842b2d2e655ce5ab62",
+    "url": "/creative-theme/index.html"
+  },
+  {
+    "revision": "ded265b349777dc1a5af",
+    "url": "/creative-theme/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fb6ea83d0740434bb931",
+    "url": "/creative-theme/static/js/2.886a7357.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/creative-theme/static/js/2.886a7357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded265b349777dc1a5af",
+    "url": "/creative-theme/static/js/main.10f75b26.chunk.js"
+  },
+  {
+    "revision": "51e470ac61e0d47390d1",
+    "url": "/creative-theme/static/js/runtime-main.4af0f157.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/creative-theme/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/contents/About.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div className="col-home"> 
+            <h1 className="subtopic">About Me</h1>
+            <p className="about"><center>
+            <h4>Hey there,</h4>
+            <h1>I am Zary G. Manning</h1>
+            <h3>Front End Web <u>Developer</u> | UI/UX <u>Designer</u></h3>
+            </center></p>
+            <br></br>
+            <p className="about">
+            For the love of design and the love of learning, software development is a passion.
+            <br></br>
+            <br></br>
+            Reading about new technologies, solving once impossible problems, providing value to a company, these are the things that fuel my desire for personal growth.
+            <br></br>
+            <br></br>
+            To me, this career is about more than building websites. Rather, I see this career as one that provides a medium between business and the humanities. Having studied Anthropology at University, I must say, I find myself especially fond of the following quote:
+            <br></br>
+            <br></br>
+            “… technology alone is not enough… it’s technology married with liberal arts, married with the humanities, that yields us the results that make our heart sing.”
+            <br></br>
+            <br></br>
+            Steve Jobs was right. Technology will continue to make unknown things possible, it will continue to make our hearts sing. My hope is that my own heart can play a small role in making that technology both easy to use and beautiful. 
+            </p>
+            
+            </div>
+            )
+        }
+    }
+    
+export default About
+    
