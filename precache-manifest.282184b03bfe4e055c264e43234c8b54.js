f52453ac5feb6d9ff0b944a4cd1e139030e976c9
zm00622 <zarymanning@gmail.com>
2020-02-18 18:33:35 -0500

    Updates

---
+++ precache-manifest.282184b03bfe4e055c264e43234c8b54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caf4ad6089b2fb50ede1f144baed15a",
+    "url": "/creative-theme/index.html"
+  },
+  {
+    "revision": "5a2319262128766b4c77",
+    "url": "/creative-theme/static/css/main.1929e2a7.chunk.css"
+  },
+  {
+    "revision": "8587d8404d844be0813d",
+    "url": "/creative-theme/static/js/2.6f9b80ae.chunk.js"
+  },
+  {
+    "revision": "2bcf73e3839e96bb488d3fc4da7ab52a",
+    "url": "/creative-theme/static/js/2.6f9b80ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2319262128766b4c77",
+    "url": "/creative-theme/static/js/main.75aacfe0.chunk.js"
+  },
+  {
+    "revision": "51e470ac61e0d47390d1",
+    "url": "/creative-theme/static/js/runtime-main.4af0f157.js"
+  },
+  {
+    "revision": "188fbd6a6114b14d8aba002cfcc6da74",
+    "url": "/creative-theme/static/media/MyPic.188fbd6a.jpg"
+  },
+  {
+    "revision": "3c3654335367b015b090c8ecb1094163",
+    "url": "/creative-theme/static/media/creative-blur.3c365433.png"
+  }
+]);
